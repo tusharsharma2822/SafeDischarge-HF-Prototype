c16d591910f501bcb3a01b07e5343c204f373293
Tushar Sharma <tusharsharma2822@gmail.com>
2026-02-25 21:00:05 +0530

    update app.js to handle backend functionality and fix minor issues

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config.js";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+
+app.get((req,res) => {
+    res.send("Greetings from the team of the SafeDischarge");
+})
+
+export default app;
