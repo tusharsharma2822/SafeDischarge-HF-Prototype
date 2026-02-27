1cf2fbb1072a53be5aa7069078602b71b0f39a8c
Tushar Sharma <tusharsharma2822@gmail.com>
2026-02-27 12:41:59 +0530

    update error.middleware.js to handle the error while generating summary

---
+++ backend/middlewares/error.middleware.js
@@ -0,0 +1,9 @@
+const errorHandler = (err, req, res, next) => {
+    console.error("Error: ", err.message);
+
+    res.status(500).json({
+        error: "Internal Server Error"
+    });
+}
+
+export default errorHandler;
