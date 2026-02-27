a6ac30b2890ebd60979c61a8dab059406852245b
Tushar Sharma <tusharsharma2822@gmail.com>
2026-02-27 12:35:37 +0530

    update app.js to handle backend functionality and fix minor issues

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config.js";
 import cors from "cors";
 import morgan from "morgan";
+import generateRoute from "./routes/generate.routes.js";
+import errorHandler from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -9,9 +11,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/generate", generateRoute);
 
-app.get(("/"), (req,res) => {
-    res.send("Greetings from the team of the SafeDischarge");
-})
+//Health Check
+app.get("/health", (req, res) => {
+    res.json({ status: "OK" });
+});
+
+app.use(errorHandler);
+
+// app.get(("/"), (req, res) => {
+    // res.send("Greetings from the team of the SafeDischarge");
+// })
 
 export default app;
